972edd165a8ed7228659f4dffb19da2c03a9cce0
Kim <jun7019@naver.com>
2025-06-09 21:08:06 +0900

    닉네임 생성 시 성별과 나이

--- app/src/main/java/kr/ac/jbnu/jun/mobileprojectgit/model/SleepRecord.kt
+++ app/src/main/java/kr/ac/jbnu/jun/mobileprojectgit/model/SleepRecord.kt
@@ -2,6 +2,9 @@ package kr.ac.jbnu.jun.mobileprojectgit.model
 
 data class SleepRecord(
     val nickname: String = "",
+    val userId: String = "",
+    val gender: String = "",
+    val age: Long = 0L,
     val startTime: String = "",
     val endTime: String = "",
     val duration: Long = 0L,
