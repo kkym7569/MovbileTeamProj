f8d263c28a29e0398eba2905cdcfc1cce5a39962
User <bg7754@naver.com>
2025-06-18 00:24:03 +0900

    개인통계

--- app/src/main/java/kr/ac/jbnu/jun/mobileprojectgit/model/Stat.kt
+++
@@ -1,8 +0,0 @@
-package kr.ac.jbnu.jun.mobileprojectgit.model
-
-import com.google.gson.annotations.SerializedName
-
-data class SleepEfficiencyStat(
-    @SerializedName("sleep_efficiency")
-    val sleepEfficiency: Float? // ← nullable로 바꿔줌
-)
